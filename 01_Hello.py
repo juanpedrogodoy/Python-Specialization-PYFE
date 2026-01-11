1b4654943edae06b42bf204c3634c45b3300af33
Juan Pedro Godoy <juanpedrogodoy1988@gmail.com>
2026-01-11 15:15:56 +0100

    Update and rename Hello.py to 01_Hello.py
    Minor changes

---
+++ 01_Hello.py
@@ -0,0 +1,10 @@
+#Project: The Polyglot Hello World
+#Description: My very first script, greeting the world in the 4 languages I speak.
+#Languages: Spanish, English, Portuguese, and German.
+#Evolution: Level 1 - The Journey Begins.
+
+
+print("Hola gente")
+print("Hello people")
+print("Bom dia gente")
+print("Hallo zusammen")
